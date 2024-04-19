375f0af0cbd8f6557d37083cceefce5a44531fa7
Amine Lamuadni <W7equwE5@gmail.com>
2024-04-19 22:04:16 +0100

    Add 101-safely_get_value.py file

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -2,22 +2,26 @@
 """Module for safely retrieving a value from a dictionary with a default."""
 
 
-from typing import TypeVar, Mapping, Any, Union, Optional
+from typing import TypeVar, Mapping, Any, Union
 
-T = TypeVar('T')  # Declare TypeVar for return and default types
+T = TypeVar('T')  # Generic type variable for flexible typing
 
 
 def safely_get_value(dct: Mapping[Any, T], key: Any,
-                     default: Optional[T] = None) -> Union[T, None]:
-    """Retrieve value from a dictionary based on given key or return default.
+                     default: Union[T, None] = None) -> Union[Any, T]:
+    """Retrieve value from a dictionary based on a given key or return default.
 
     Args:
         dct (Mapping[Any, T]): The dictionary from which to retrieve the value.
         key (Any): The key to look for in the dictionary.
-        default (Optional[T]): The default value to return if the key is not
+        default (Union[T, None]): The default value to return if the key is not
         found.
 
     Returns:
-        Union[T, None]: The value associated with the key, or the default.
+        Union[Any, T]: The value associated with the key, or the default. This
+        allows for returning any type.
     """
-    return dct.get(key, default)
+    if key in dct:
+        return dct[key]
+    else:
+        return default
