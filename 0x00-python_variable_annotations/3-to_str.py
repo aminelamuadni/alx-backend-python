9b129b451ba921046bde85e1524ea7608b3b5cbd
Amine Lamuadni <W7equwE5@gmail.com>
2024-04-19 17:52:06 +0100

    Add 3-to_str.py file

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module for converting a float to its string representation."""
+
+
+def to_str(n: float) -> str:
+    """Convert a float to a string.
+
+    Args:
+        n (float): The float to convert.
+
+    Returns:
+        str: The string representation of the float.
+    """
+    return str(n)
