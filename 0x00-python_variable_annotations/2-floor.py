5040940e882e6cb8c75acab23a0b625849089d71
Amine Lamuadni <W7equwE5@gmail.com>
2024-04-19 17:49:55 +0100

    Add 2-floor.py file

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import math
+"""Module for computing the floor of a float."""
+
+
+def floor(n: float) -> int:
+    """Compute the floor of a float and return it as an integer.
+
+    Args:
+        n (float): The float number to compute the floor of.
+
+    Returns:
+        int: The floor of the float.
+    """
+    return math.floor(n)
